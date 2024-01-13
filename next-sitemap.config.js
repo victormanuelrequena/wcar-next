2b7fff1ccccfa0df29c26266643e4fc0a1717fc8
victormanuelrequena <requenadev1@gmail.com>
2024-01-13 14:04:59 -0400

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://https://desarrollodesitiosweb.co",
+  siteUrl: process.env.SITE_URL || "https://desarrollodesitiosweb.co",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
